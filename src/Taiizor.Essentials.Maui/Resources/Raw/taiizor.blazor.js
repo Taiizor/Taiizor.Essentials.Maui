199514c7ab633cc6a0c926abc51cc98ee2d7c1d7
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-09-23 00:26:54 +0300

    Infrastructure Preparation 26

--- src/Taiizor.Essentials.Maui/Resources/Raw/taiizor.blazor.js
+++ src/Taiizor.Essentials.Maui/Resources/Raw/taiizor.blazor.js
@@ -217,3 +217,21 @@ function InvokeReferenceMethodAsync(objectReference, methodName, arguments) {
 
     //objectReference.dispose();
 }
+
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
+}
+
+if (!String.format) {
+    String.format = function(format) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return format.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
+}
