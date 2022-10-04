af70ddd3982692d1229ab7a7172706fb9f5aa8e4
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-04 17:48:42 +0300

    Infrastructure Preparation 51

--- src/Taiizor.Essentials.Maui.AppCenter/Platforms/Android/PlatformTest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Taiizor.Essentials.Maui.AppCenter.Platforms
-{
-    // All the code in this file is only included on Tizen.
-    public class PlatformTest
-    {
-        public static string Test = "Empty";
-    }
-}
