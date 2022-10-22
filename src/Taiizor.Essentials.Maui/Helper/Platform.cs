c19a90547b3f799418028b0311db9b865a082107
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-22 22:34:32 +0300

    Update Platform.cs

--- src/Taiizor.Essentials.Maui/Helper/Platform.cs
+++ src/Taiizor.Essentials.Maui/Helper/Platform.cs
@@ -4,6 +4,17 @@ namespace Taiizor.Essentials.Maui.Helper
 {
     public class Platform
     {
-        public static PlatformEnum GetPlatform => PlatformEnum.Android;
+        public static PlatformEnum GetPlatform
+        {
+#if MACCATALYST
+            get => PlatformEnum.MacCatalyst;
+#elif ANDROID
+            get => PlatformEnum.Android;
+#elif IOS
+            get => PlatformEnum.iOS;
+#else
+            get => PlatformEnum.WinUI;
+#endif
+        }
     }
 }
