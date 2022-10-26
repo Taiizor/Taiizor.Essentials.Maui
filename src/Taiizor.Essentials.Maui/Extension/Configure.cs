3b5f35dae7342ad30e61c348aec7cfa6fc92506c
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-10-26 15:46:16 +0300

    Infrastructure Preparation 86

---
+++ src/Taiizor.Essentials.Maui/Extension/Configure.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Maui.LifecycleEvents;
+
+#if WINDOWS
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
+#elif ANDROID
+using Android.Views;
+#endif
+
+namespace Taiizor.Essentials.Maui.Extension
+{
+    public static class Configure
+    {
+        public static void UseFullscreen(this MauiAppBuilder Builder)
+        {
+            Builder.ConfigureLifecycleEvents(events =>
+            {
+#if WINDOWS
+                events.AddWindows(windows =>
+                {
+                    windows.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false;
+                        
+                        var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        var id = Win32Interop.GetWindowIdFromWindow(handle);
+                        var appWindow = AppWindow.GetFromWindowId(id);
+                        
+                        switch (appWindow.Presenter)
+                        {
+                            case OverlappedPresenter overlappedPresenter:
+                                overlappedPresenter.SetBorderAndTitleBar(false, false);
+                                overlappedPresenter.Maximize();
+                                break;
+                        }
+                    });
+                });
+#elif ANDROID
+                events.AddAndroid(android => android
+                    .OnCreate((activity, bundle) => {
+                        activity.Window?.AddFlags(WindowManagerFlags.Fullscreen);
+                    })
+                );
+//#elif IOS || MACCATALYST
+//                events.AddiOS(ios => ios
+//                    .OnActivated((app) => LogEvent("OnActivated"))
+//                    .OnResignActivation((app) => LogEvent("OnResignActivation"))
+//                    .DidEnterBackground((app) => LogEvent("DidEnterBackground"))
+//                    .FinishedLaunching((app, dict) => AppCenterService.Engine(Keys, Watch)) //Start //LogEvent("FinishedLaunching", $"{dict}")
+//                    .WillTerminate((app) => LogEvent("WillTerminate"))); //End
+#endif
+            });
+        }
+    }
+}
