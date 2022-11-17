044a662ca88e5dfec828f2fad6063b009cbda55f
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-11-17 22:58:52 +0300

    Infrastructure Preparation 131

---
+++ src/Taiizor.Essentials.Maui/Helper/Combine.cs
@@ -0,0 +1,10 @@
+﻿namespace Taiizor.Essentials.Maui.Helper
+{
+    internal class Combine
+    {
+        public static string FullPath(string Directory, string File, string Extension)
+        {
+            return Path.Combine(Directory, $"{File}{Extension}");
+        }
+    }
+}
