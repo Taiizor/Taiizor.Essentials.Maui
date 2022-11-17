92e65b970e19256e1c0892aadb19e5920c67f841
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-11-17 22:44:16 +0300

    Infrastructure Preparation 130

--- src/Taiizor.Essentials.Maui/Value/Internal.cs
+++ src/Taiizor.Essentials.Maui/Value/Internal.cs
@@ -6,7 +6,7 @@ namespace Taiizor.Essentials.Maui.Value
     internal class Internal
     {
         public static readonly string StorageExtension = ".json";
-        
+
         public static readonly string CacheDirectory = FileSystem.Current.CacheDirectory;
 
         public static readonly string DataDirectory = FileSystem.Current.AppDataDirectory;
